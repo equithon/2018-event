0b290baea00006251c6ae131c5334fb3c3e241d6
Ziyad AlYafi <ziyad@equithon.org>
2017-12-09 19:10:32 -0500

    Centered placeholder image within cell and added very placeholder social media buttons

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,10 +6,8 @@ import Typography from 'material-ui/Typography';
 
 import HomeAppBar from '/imports/ui/HomeAppBar.js';
 import SubText from '/imports/ui/home-components/SubText.js';
-import Image from '/imports/ui/home-components/Image.js';
 
-import StayPosted from '/imports/ui/buttons/StayPosted.js';
-
+import { StayPosted, Facebook, Twitter, Instagram} from '/imports/ui/buttons/SocialMedia.js';
 
 const theme = createMuiTheme({
   palette: {
@@ -19,21 +17,42 @@ const theme = createMuiTheme({
 });
 
 const imageStyle = {
-  // Z - This is an attempt to force the image to responsively
-  // scale with screensize but it just doesn't work.
-  // I can't seem to figure out why.
+  // Z - Image is centered in its grid cell but it won't scale
+  // screen size.
   maxHeight: '300px',
   maxWidth: '500px',
   height: '100%',
-  width: 'auto'
+  width: 'auto',
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
 };
-const mainTextStyle = {
+
+const mainTextBoxStyle = {
+  gridArea: 'rr1',
+
   display: 'grid',
   gridTemplateRows: '4fr 1fr',
   gridRowGap: '5px',
   padding: '5px'
 };
 
+const StayPostedStyle = {
+  float: 'left',
+  padding: '10px',
+}
+const FacebookButtonStyle = {
+  float: 'left',
+  padding: '10px',
+}
+const TwitterButtonStyle = {
+  float: 'left',
+  padding: '10px',
+}
+const InstagramButtonStyle = {
+  float: 'left',
+  padding: '10px',
+}
 // Main application entry point
 export default class App extends Component {
   constructor(props) {
@@ -41,14 +60,6 @@ export default class App extends Component {
   };
 
   render() {
-    const socialMediaButtons =
-      <div style={{display: 'inline'}}>
-        <StayPosted />
-        {/*<FacebookButton />
-        <TwitterButton />
-        <InstagramButton />*/}
-      </div>;
-
     return (
       <MuiThemeProvider theme={theme}>
         <div id="app">
@@ -60,14 +71,21 @@ export default class App extends Component {
             <SubText />
 
             {/* Row 1 */}
-            <Image gridArea="lr1" style={imageStyle} src="/images/placeholder.jpg" />
-            <div style={mainTextStyle}>
-              <Typography type="display2" align="left">
+            <div style={{gridArea: 'lr1', width: '100%', height: '100%'}}>
+              <img style={imageStyle} src="/images/placeholder.jpg" />
+            </div>
+            <div style={mainTextBoxStyle}>
+              <Typography color="primary" type="display2" align="left">
                 Waterloo's biggest social innovation hackathon is coming back in May 2018.
               </Typography>
-              {socialMediaButtons}
-            </div>
 
+              <div>
+                <div style={StayPostedStyle}>{ StayPosted() }</div>
+                <div style={FacebookButtonStyle}>{ Facebook() }</div>
+                <div style={TwitterButtonStyle}>{ Twitter() }</div>
+                <div style={InstagramButtonStyle}>{ Instagram() }</div>
+              </div>
+            </div>
           </div>
         </div>
       </MuiThemeProvider>
