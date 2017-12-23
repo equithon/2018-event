097cc4c57c27dd1c80069b6c307d208b06d6d80c
nathan-knight <knathanknight@gmail.com>
2017-12-23 12:46:21 -0500

    Lots of progress on the page structure

---
+++ client/ui/App.js
@@ -0,0 +1,127 @@
+import React, {Component} from 'react';
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import indigo from 'material-ui/colors/indigo';
+import Typography from 'material-ui/Typography';
+
+import HomeAppBar from '/client/ui/HomeAppBar.js';
+import SubText from '/client/ui/components/home-components/SubText.js';
+
+import {StayPosted, Facebook, Twitter, Instagram} from '/client/ui/buttons/SocialMedia.js';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: indigo,  // Placeholders until we get actual
+        secondary: purple // color palettes.
+    }
+});
+
+const imageStyle = {
+    // Z - Image is centered in its grid cell but it won't scale
+    // screen size.
+    maxHeight: '300px',
+    maxWidth: '500px',
+    height: '100%',
+    width: 'auto',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+};
+
+const mainTextBoxStyle = {
+    gridArea: 'right',
+
+    display: 'grid',
+    gridTemplateRows: 'auto auto',
+    gridRowGap: '5px',
+    padding: '5px'
+};
+
+const StayPostedStyle = {
+    float: 'left',
+    padding: '10px',
+};
+
+const FacebookButtonStyle = {
+    float: 'left',
+    padding: '10px',
+};
+
+const TwitterButtonStyle = {
+    float: 'left',
+    padding: '10px',
+};
+
+const InstagramButtonStyle = {
+    float: 'left',
+    padding: '10px',
+};
+
+// Main application entry point
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <div id="app">
+                    <HomeAppBar/>
+
+                    <div id="app-body">
+                        {/* SubText */}
+                        <div style={{gridArea: 'subtext-row', justifySelf: 'end'}}>
+                            <SubText/>
+                        </div>
+
+                        {/* Row 1 */}
+                        <div className="split-column-row" style={{gridArea: 'title-row',gridColumnGap: '20px', alignItems: 'center'}}>
+                            <div style={{gridArea: 'left', width: '100%', height: '100%'}}>
+                                <img style={imageStyle} src="/images/placeholder.jpg"/>
+                            </div>
+                            <div style={mainTextBoxStyle}>
+                                <Typography color="primary" type="display2" align="left">
+                                    Waterloo's biggest social innovation hackathon is coming back in May 2018.
+                                </Typography>
+                                <div>
+                                    <div style={StayPostedStyle}>{StayPosted()}</div>
+                                    <div style={FacebookButtonStyle}>{Facebook()}</div>
+                                    <div style={TwitterButtonStyle}>{Twitter()}</div>
+                                    <div style={InstagramButtonStyle}>{Instagram()}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Row 2*/}
+                        <div className="split-column-row" style={{gridArea: 'info-row', alignItems: 'center'}}>
+                            <div style={{gridArea: 'left'}}>
+                                <Typography color="primary" type="display1">
+                                    What is Equithon?
+                                </Typography>
+                                <Typography color="secondary" type="Body1" align="left">
+                                    Equithon is a hackathon for students to create technical projects that solve an equity issue or promote equity. We strive to provide an inclusive environment at our event where hackers can get support from industry mentors to develop their ideas and projects.
+                                </Typography>
+                            </div>
+                            <div style={{gridArea: 'right', width: '100%', height: '100%'}}>
+                                <img style={imageStyle} src="/images/placeholder.jpg"/>
+                            </div>
+                        </div>
+
+                        {/* Row 3 */}
+                        <div style={{gridArea: 'quote-row', justifySelf: 'center'}}>
+                            <Typography color="primary" type="display2" align="left" style={{whiteSpace: 'pre'}}>"Equity isn't a women's issue,<br/>   Equity is an everyone issue."</Typography>
+                            <Typography color="primary" type="headline" align="left">Feridun Hamdullahpur, President and Vice-Chancellor</Typography>
+                        </div>
+
+                        {/* Row 4*/}
+                        <div style={{gridArea: 'faq-row', justifySelf: 'center'}}>
+
+                        </div>
+
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
--- client/ui/components/home-components/SubText.js
+++ client/ui/components/home-components/SubText.js
@@ -8,17 +8,17 @@ const cellStyle = {
   margin: '0 0 0 auto',
   paddingRight: '10px',
   gridArea: 'st'
-}
+};
 const leftTextStyle = {
   float: 'left',
   width: '125px',
   paddingRight: '10px'
-}
+};
 const rightTextStyle = {
   float: 'left',
   width: '125px',
   paddingLeft: '10px'
-}
+};
 
 /*
  * A small subtext rendered on the top-right after the AppBar.
@@ -36,7 +36,7 @@ class SubText extends Component {
       height: '50px',
       position: 'relative',
       float: 'left'
-    }
+    };
 
     return(
       <div style={cellStyle}>
