f80ec8eb23c753c984704dfd387dd94ebffe7c6c
Ziyad AlYafi <ziyad@equithon.org>
2018-01-14 20:28:13 -0500

    Fixed logo and made appbar smaller on mobile screens

--- client/ui/components/HomeAppBar.js
+++ client/ui/components/HomeAppBar.js
@@ -10,13 +10,13 @@ import Toolbar from 'material-ui/Toolbar';
  * We would add buttons etc. to this as we add new features.
  */
 export default class HomeAppBar extends Component {
-  render() {
-    return (
-      <AppBar id="AppBar" position="fixed" color="inherit">
-        <Toolbar>
-          <img className="equithon-logo" src="/logos/Logo.png" />
-        </Toolbar>
-      </AppBar>
-    );
-  }
+    render() {
+        return (
+            <AppBar id="appbar" position="fixed" color="inherit">
+                <Toolbar>
+                    <img className="equithon-logo" src="/logos/logo-bulb_417x417.png" />
+                </Toolbar>
+            </AppBar>
+        );
+    }
 }
