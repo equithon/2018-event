fb29f76c6d14db92d9e677deb81462c67bf619e9
Ziyad AlYafi <ziyad@equithon.org>
2018-01-26 21:32:10 -0500

    Customize email text.

--- imports/api/user/methods.js
+++ imports/api/user/methods.js
@@ -14,9 +14,30 @@ export const sendVerificationEmail = new ValidatedMethod({
     }).validator(),
 
     run(emailObj) {
-        let user = Accounts.findUserByEmail(emailObj.email);
+        let user = Meteor.user();
 
-        if (user) Accounts.sendVerificationEmail(user._id, emailObj.email);
+        /* Customize email */
+        Accounts.emailTemplates.siteName = 'Equithon';
+        Accounts.emailTemplates.from = 'Equithon Hello <hello@equithon.org>';
+
+        Accounts.emailTemplates.verifyEmail = {
+            subject() {
+                return "Equithon - Confirm your email address";
+            },
+            text(user, url) {
+                return `Congratulations ${user.username}!
+
+You are one step closer to applying to Equithon 2018. To finish creating your Equithon account, please click the link below to verify your email address.
+
+${url}
+
+Thanks,
+Equithon Team
+`;
+            },
+        };
+
+        if (user) Accounts.sendVerificationEmail(user.username);
         else throw new Meteor.error('sendVerificationEmail.user_not_found', 'Could not find user with that email address');
     },
 });
