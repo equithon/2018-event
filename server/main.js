5f9c9e91f860b723878626e7b123476dc17ee39a
Ziyad AlYafi <ziyad@equithon.org>
2018-01-30 10:21:35 -0500

    Use username and password for SMTP requests to AWS SES

--- server/main.js
+++ server/main.js
@@ -7,7 +7,14 @@ import Applications from '/imports/api/applications/applications.js';
 
 
 Meteor.startup(() => {
-    // Deny all client-side updates on the EmailSubscriptions collection
+    // Configure MAIL_URL using AWS SES SMTP service
+    let smtpusername = encodeURIComponent("AKIAJWZSYASZKAUUE7NQ");
+    let smtppassword = encodeURIComponent("Aurq/HxciqOHxj4XOkxVQbSoNOK1vH5/1oVGcbziwBZb");
+    let smtphostname = encodeURIComponent("email-smtp.us-east-1.amazonaws.com");
+
+    process.env.MAIL_URL = "smtp://" + smtpusername + ":" + smtppassword + "@" + smtphostname + ":587";
+
+    // Deny all client-side updates on our collections.
     EmailSubscriptions.deny({
       insert() { return true; },
       update() { return true; },
