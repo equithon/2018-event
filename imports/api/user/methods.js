d0967c662e84a81d1704903df515afcc14dac148
Ziyad AlYafi <ziyad@equithon.org>
2018-02-09 15:44:10 -0500

    Move email templating to accounts.js which makes more sense

--- imports/api/user/methods.js
+++ imports/api/user/methods.js
@@ -61,7 +61,7 @@ export const sendVerificationLink = new ValidatedMethod({
     run({ captchaToken }) {
         if (!this.userId) throw new Meteor.Error('user.sendVerificationLink.unauthorized',
                 'You need to be logged in to send verification emails');
-        
+
         // Get user and relevant attributes.
         let user = Meteor.user();
         let verificationTokens = user.services.email.verificationTokens;
@@ -85,59 +85,3 @@ export const sendVerificationLink = new ValidatedMethod({
         }
     }
 });
-
-
-/***** Email Customization *****/
-/*
- * Customize password recovery email.
- */
-Accounts.emailTemplates.siteName = "Equithon";
-Accounts.emailTemplates.from = "Equithon <hello@equithon.org>";
-Accounts.emailTemplates.resetPassword = {
-    subject(user) {
-        return "Equithon - Reset your password";
-    },
-
-    text(user, url) {
-        url = url.replace('#/', 'accounts/'); // This is important as it allows us to route the link ourselves.
-        url = url.replace('equithon-website-production.us-east-2.elasticbeanstalk.com/', 'equithon.org/');
-
-        return `
-You are receiving this email because you requested a password reset.
-Please click the link below to reset your password.
-
-${url}
-
-If you didn't request this email, please ignore it.
-Thanks,
-The Equithon Team.
-`
-    },
-};
-
-/*
- * Customize verification email.
- */
-Accounts.emailTemplates.siteName = "Equithon";
-Accounts.emailTemplates.from = "Equithon <hello@equithon.org>";
-Accounts.emailTemplates.verifyEmail = {
-    subject(user) {
-        return "Equithon - Confirm your email address";
-    },
-
-    text(user, url) {
-        url = url.replace('#/', 'accounts/'); // This is important as it allows us to route the link ourselves.
-        url = url.replace('equithon-website-production.us-east-2.elasticbeanstalk.com/', 'equithon.org/');
-
-        return `
-Congratulations ${user.firstName} ${user.lastName}! You are one step closer to Equithon 2018.
-To finish creating your Equithon account, please click the link below to verify your email address.
-
-${url}
-
-If you didn't request this email, please ignore it.
-Thanks,
-The Equithon Team.
-`
-    },
-};
